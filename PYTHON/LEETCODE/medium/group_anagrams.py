5710cb192196518731d53fe1fdb13a31f4d7bdb1
javier <javier_2790@outlook.com>
2024-12-28 12:54:59 +0100

    feat: add more medium leetcode

--- PYTHON/LEETCODE/medium/group_anagrams.py
+++ PYTHON/LEETCODE/medium/group_anagrams.py
@@ -2,18 +2,18 @@
 anagrams
  together. You can return the answer in any order.'''
 
+from collections import defaultdict
+
 def valid_anagrams(strs: list):
-    ans = []
-
-    for w in range(len(strs)):
-        actual = set(strs[w])
-        res = []
-        for ws in range(len(strs)):
-            if actual == set(strs[ws]):
-                res.append(strs[ws])
-        ans.append(res)
-
-    return ans
+    ans = defaultdict(list)
+    
+    for w in strs:
+        count = [0] * 26
+        for ch in w:
+            count[ord(ch) - ord('a')] += 1
+        ans[tuple(count)].append(w)
+    
+    return [val for val in ans.values()]
 
 
 strs = ["eat","tea","tan","ate","nat","bat"]
---
+++ PYTHON/LEETCODE/medium/longest_palindromic_substring.py
@@ -0,0 +1,16 @@
+'''Given a string s, return the longest 
+palindromic
+ 
+substring
+ in s.'''
+
+def longest_palindromic_subs(s: str):
+    res = ''
+    l = 0
+    for i in range(len(s)):
+        # odd length
+        
+
+s = "hgabahb"
+
+print(longest_palindromic_subs(s))
